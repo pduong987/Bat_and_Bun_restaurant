0c19092fe615c4987ca1d7b0ce9a61247cae33c8
Jessica Gozali <79122217+petitejess@users.noreply.github.com>
2021-11-30 15:43:38 +1100

    Add route to HomeView

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+import HomeView from './views/HomeView'
+
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
+      <main>
+        <Routes>
+          <Route exact path='/' element={<HomeView />} />
+        </Routes>
+      </main>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
