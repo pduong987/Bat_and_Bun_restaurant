44b7e6e2fb7cc6fc4a24bf9ca0df426e7412abb2
Jessica Gozali <79122217+petitejess@users.noreply.github.com>
2021-12-07 09:55:51 +1100

    Test initial deployment

--- server/index.js
+++ server/index.js
@@ -1,24 +1,30 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
+import databaseConnector from './database.js';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/batnbun';
+const PORT = process.env.PORT || 5000;
+const HOST = '0.0.0.0';
+const __dirname = path.resolve();
 
 dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 5000;
-// Import the database connection function
-import databaseConnector from './database.js';
-// Establish what the database URL is going to be
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/batnbun';
-// Connect to the database using the URL
 databaseConnector(MONGODB_URI).then(() => {
     console.log("Database connected successfully!");
 }).catch(error => {
     console.log(`
     Some error occured connecting to the database! It was: 
     ${error}
-    `)
+    `);
 });
 
+// PRODUCTION: Serve static build client
+app.use(express.static(path.join(__dirname, '/client/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  }
+);
 
-app.listen(PORT, console.log(`Server running on port ${PORT}`));
+app.listen(PORT, HOST, console.log(`Server listening at http://${HOST == "0.0.0.0" && "localhost"}:${PORT}/`));
