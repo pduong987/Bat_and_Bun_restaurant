21b6d24371b26f270009d61a5e5b401b400ff11d
Jessica Gozali <79122217+petitejess@users.noreply.github.com>
2021-12-17 22:21:44 +1100

    Move CartContext up, refactor Cart Table, add logic for navigation to Order Confirmation View when cart is empty

--- client/src/views/CartView.js
+++ client/src/views/CartView.js
@@ -1,24 +1,56 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import {
   Container,
   Typography,
   Button
 } from '@mui/material';
+
+import {
+  CART_ADD,
+  CART_REMOVE
+} from '../reducers/constants';
+import { CartContext } from '../contexts/CartContext';
 import CartTable from '../components/CartTable';
 
 const CartView = () => {
+  const { cartItems, dispatch } = useContext(CartContext);
+  const emptyMessage = "Your cart is empty. Try adding some items.";
+  const [isEmpty, setIsEmpty] = useState(true);
+
+  useEffect(()=> {
+    setIsEmpty(cartItems.length < 1);
+  }, [cartItems]);
+
+  const deleteFromCart = (item) => {
+    dispatch({type: CART_REMOVE, payload: item})
+  };
+
+  const addQty = (itemName) => {
+    dispatch({type: CART_ADD, payload: {name: itemName, qty: 1}})
+  };
+
+  const minQty = (itemName) => {
+    dispatch({type: CART_ADD, payload: {name: itemName, qty: -1}})
+  };
+
   return (
     <div id="CartView">
       <Container>
         <Typography variant="h1" sx={{ fontSize: '3em', textAlign: 'center' }}>Cart</Typography>
       </Container>
 
-      <CartTable />
+      <CartTable
+        emptyMessage={emptyMessage}
+        cartItems={cartItems}
+        deleteFromCart={deleteFromCart}
+        addQty={addQty}
+        minQty={minQty}
+      />
 
       <Container className="cart-buttons">
         <Button variant="outlined" component={Link} to="/">Continue Shopping</Button>
-        <Button variant="contained" component={Link} to="/order-confirmation">Place Order</Button>
+        <Button variant="contained" component={Link} to="/order-confirmation" disabled={isEmpty}>Place Order</Button>
       </Container>
     </div>
   )
--- client/src/views/OrderConfirmationView.js
+++ client/src/views/OrderConfirmationView.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import {
   Container,
   Typography
 } from '@mui/material';
 import CartTable from '../components/CartTable';
 import CustomerDetailsForm from '../components/OrderConfirmationView/CustomerDetailsForm';
+import { CartContext } from '../contexts/CartContext';
 
 const OrderConfirmationView = () => {
+  const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    cartItems.length < 1 && navigate('/cart');
+  }, [cartItems]);
+
   return (
     <div id="OrderConfirmationView">
       <Container>
         <Typography variant="h1" sx={{ fontSize: '3em', textAlign: 'center' }}>Order Confirmation</Typography>
       </Container>
 
-      <CartTable />
+      <CartTable
+        cartItems={cartItems}
+      />
 
       <CustomerDetailsForm />
     </div>
