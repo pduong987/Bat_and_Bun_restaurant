402823c17e77138012cf21bfab244f724f98e6a2
Duong Pham <ptduong987@gmail.com>
2022-01-05 11:38:45 +0000

    Fixed bugs causing undefined. Need to check value exists first

--- client/src/components/HomeView/MenuItemDetail.js
+++ client/src/components/HomeView/MenuItemDetail.js
@@ -1,5 +1,5 @@
-import React, { useContext, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import {
   Card,
   CardMedia,
@@ -7,15 +7,13 @@ import {
   CardActions,
   Typography,
   IconButton,
-  Button
-} from '@mui/material';
-import RemoveIcon from '@mui/icons-material/Remove';
-import AddIcon from '@mui/icons-material/Add';
+  Button,
+} from "@mui/material";
+import RemoveIcon from "@mui/icons-material/Remove";
+import AddIcon from "@mui/icons-material/Add";
 
-import {
-  CART_ADD
-} from '../../reducers/constants.js';
-import { CartContext } from '../../contexts/CartContext';
+import { CART_ADD } from "../../reducers/constants.js";
+import { CartContext } from "../../contexts/CartContext";
 
 const MenuItemDetail = ({ item }) => {
   const { dispatch } = useContext(CartContext);
@@ -29,72 +27,55 @@ const MenuItemDetail = ({ item }) => {
         name: item.name,
         qty: qtyDisplay,
         price: Number(item.price),
-        item: item._id
-      }
+        item: item._id,
+      },
     });
-    
+
     // Redirect to Cart View
-    navigate('/cart');
+    navigate("/cart");
   };
 
   return (
-    <Card
-      className="item-detail-card"
-      elevation={0}
-    >
-      <CardMedia
-        component="img"
-        image={item.image}
-        alt={item.name}
-      />
+    <Card className="item-detail-card" elevation={0}>
+      <CardMedia component="img" image={item.image} alt={item.name} />
       <CardContent>
-        <Typography variant="h4">
-          {item.name}
-        </Typography>
-        <Typography
-          variant="body1"
-        >
-          {item.notes && item.notes}
-        </Typography>
-        <Typography
-          variant="body2"
-        >
-          ${item.price.toFixed(2)}
+        <Typography variant="h4">{item.name}</Typography>
+        <Typography variant="body1">{item.notes && item.notes}</Typography>
+        <Typography variant="body2">
+          ${item.price ? item.price.toFixed(2) : "Unknown"}
         </Typography>
       </CardContent>
       <CardActions>
         <div>
-          {qtyDisplay > 1
-          ? (
-              <IconButton
-                aria-label="reduce quantity"
-                className="min-btn"
-                onClick={() => qtyDisplay > 1 && setQtyDisplay(qtyDisplay - 1)}
-              >
-                <RemoveIcon />
-              </IconButton>
-            )
-          : (
-              <span style={{display: 'inline-block', width: '40px', height: '20px'}}></span>
-            )
-          }
-          
+          {qtyDisplay > 1 ? (
+            <IconButton
+              aria-label="reduce quantity"
+              className="min-btn"
+              onClick={() => qtyDisplay > 1 && setQtyDisplay(qtyDisplay - 1)}
+            >
+              <RemoveIcon />
+            </IconButton>
+          ) : (
+            <span
+              style={{ display: "inline-block", width: "40px", height: "20px" }}
+            ></span>
+          )}
+
           <span className="qty-number">{qtyDisplay}</span>
 
-          {qtyDisplay < 10
-          ? (
-              <IconButton
-                aria-label="add quantity"
-                className="add-btn"
-                onClick={() => qtyDisplay < 10 && setQtyDisplay(qtyDisplay + 1)}
-              >
-                <AddIcon />
-              </IconButton>
-            )
-          : (
-              <span style={{display: 'inline-block', width: '40px', height: '20px'}}></span>
-            )
-          }
+          {qtyDisplay < 10 ? (
+            <IconButton
+              aria-label="add quantity"
+              className="add-btn"
+              onClick={() => qtyDisplay < 10 && setQtyDisplay(qtyDisplay + 1)}
+            >
+              <AddIcon />
+            </IconButton>
+          ) : (
+            <span
+              style={{ display: "inline-block", width: "40px", height: "20px" }}
+            ></span>
+          )}
         </div>
         <Button
           variant="contained"
@@ -105,7 +86,7 @@ const MenuItemDetail = ({ item }) => {
         </Button>
       </CardActions>
     </Card>
-  )
-}
+  );
+};
 
 export default MenuItemDetail;
