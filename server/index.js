f8adc2448b0de16e162741a77d30f421c574d277
Duong Pham <ptduong987@gmail.com>
2022-01-05 11:24:06 +0000

    Changed port as MacOS Montery uses port 5000 for its Control Center and breaks the app on mac

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
-const path = require('path');
-const cors = require('cors');
-const express = require('express');
-const dotenv = require('dotenv');
+const path = require("path");
+const cors = require("cors");
+const express = require("express");
+const dotenv = require("dotenv");
 dotenv.config();
-const databaseConnector = require('./database');
-const itemRoutes = require('./routes/itemRoutes');
-const orderRoutes = require('./routes/orderRoutes.js');
+const databaseConnector = require("./database");
+const itemRoutes = require("./routes/itemRoutes");
+const orderRoutes = require("./routes/orderRoutes.js");
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/batnbun';
-const PORT = process.env.PORT || 5000;
-const HOST = '0.0.0.0';
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/batnbun";
+const PORT = process.env.PORT || 5999;
+const HOST = "0.0.0.0";
 
 path.resolve();
 
@@ -17,27 +18,34 @@ const app = express();
 
 app.use(cors());
 
-databaseConnector(MONGODB_URI).then(() => {
+databaseConnector(MONGODB_URI)
+  .then(() => {
     console.log("Database connected successfully!");
-}).catch(error => {
+  })
+  .catch((error) => {
     console.log(`
     Some error occured connecting to the database! It was: 
     ${error}
     `);
-});
+  });
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 // ROUTES
-app.use('/items', itemRoutes);
-app.use('/orders', orderRoutes);
+app.use("/items", itemRoutes);
+app.use("/orders", orderRoutes);
 
 // PRODUCTION: Serve static build client
-app.use(express.static(path.join(__dirname, '/client/build')));
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  }
-);
+app.use(express.static(path.join(__dirname, "/client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
-app.listen(PORT, HOST, console.log(`Server listening at http://${HOST == "0.0.0.0" && "localhost"}:${PORT}/`));
+app.listen(
+  PORT,
+  HOST,
+  console.log(
+    `Server listening at http://${HOST == "0.0.0.0" && "localhost"}:${PORT}/`
+  )
+);
