b876ffeab2d2db5a3b3fe3aec3120bf0edea5195
Jessica Gozali <79122217+petitejess@users.noreply.github.com>
2022-01-10 23:08:21 +1100

    Change Stripe Element to take only cc no, exp, cvc, and fix margin style

---
+++ client/src/components/DashboardView/UploadImage.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+
+const UploadImage = () => {
+  const [fileData, setFileData] = useState();
+
+  const fileChangeHandler = (e) => {
+    setFileData(e.target.files[0]);
+  };
+
+  const onSubmitHandler = (e) => {
+    console.log("file data ====>", fileData);
+    e.preventDefault();
+
+    const data = new FormData();
+    data.append("image", fileData); // image key to use in Postman
+
+    const server = "http://localhost:5999/upload";
+
+    console.log(`data: ${data}`);
+
+    // Send reqest to backend - Single upload
+    fetch(`${server}/single`, {
+      method: "POST",
+      body: data,
+    })
+      .then((result) => {
+        console.log("File sent successfully", result);
+      })
+      .catch((err) => {
+        console.log("Something Went Wrong", err);
+      });
+  };
+
+  return (
+    <div>
+      <h1>Uploading Files using Multer</h1>
+      <form onSubmit={onSubmitHandler}>
+        <input type="file" onChange={fileChangeHandler} />
+        <br />
+        <br />
+        <button type="submit">Send to backend</button>
+      </form>
+      <br />
+      <img
+        src="http://localhost:5999/images/1636891838696-XEBIA 1.jpeg"
+        alt="S3 file"
+        width="800"
+      />
+    </div>
+  );
+}
+
+export default UploadImage
