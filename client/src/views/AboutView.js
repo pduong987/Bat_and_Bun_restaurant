e4464941f36ced310f49b0229646d342a03cfe67
Jessica Gozali <79122217+petitejess@users.noreply.github.com>
2022-01-11 15:12:03 +1100

    Add about content

--- client/src/views/AboutView.js
+++ client/src/views/AboutView.js
@@ -11,14 +11,14 @@ const AboutView = () => {
       <Container>
         <Typography variant="h1" sx={{ fontSize: '3em' }}>Bat + Bun</Typography>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis volutpat neque. Morbi ultricies leo at elit congue, sit amet bibendum metus molestie. Etiam ornare quis sapien vel efficitur. Nullam quis nunc semper, facilisis metus eu, congue magna. Praesent congue quis odio et luctus. Donec a est vitae erat elementum dictum. Sed pretium placerat enim ut sodales. Vestibulum dictum mauris a semper tempus. Morbi ac arcu ex. Morbi facilisis urna at pharetra iaculis. Duis in quam eu erat malesuada eleifend vitae feugiat libero. Nam consectetur risus ut sapien lacinia maximus. In malesuada mi at dui posuere, et varius turpis eleifend. Phasellus quis nunc eu nulla rutrum accumsan eget non nisl.
+          Bat + Bun (Bowl + Noodle) is offering the best Vietnamese cuisine located in Darlinghurst area. Here, you will be able to taste delicious pork belly, nicely cooked noodle salads, and other Vietnamese Street food. You will also hardly forget the delicious tea and sweet treats that you can order.
         </p>
         <div className="about-owner">
           <p className="about-img">
             <img src="./img/owner.jpg" alt="Bat + Bun owner" />
           </p>
           <p>
-            Donec nunc nulla, molestie rutrum neque congue, porta dictum turpis. Etiam quis laoreet ligula. In ut nisl in enim dapibus commodo. Curabitur viverra eleifend dui, at ultricies leo fermentum euismod. Maecenas volutpat ex a enim sagittis pretium. Maecenas nulla massa, iaculis eu imperdiet a, ultrices eget tellus. Etiam ipsum augue, fringilla sit amet mi vitae, rhoncus rhoncus ligula. Curabitur sit amet tellus id massa hendrerit tincidunt. Vivamus accumsan massa sed nisi vulputate, non tempus nunc pellentesque. Phasellus ac facilisis nibh. Etiam in urna quis quam egestas feugiat.
+            We love to give the best service and the best quality of food for everyone. Fresh ingredients and selected quality herbs and spices are our secret in delivering the best flavour our food can offer. So, the next time you come to Darlinghurst, do not forget to say hi! We will be waiting for you with our delicious food in Bat + Bun...
           </p>
         </div>
       </Container>
